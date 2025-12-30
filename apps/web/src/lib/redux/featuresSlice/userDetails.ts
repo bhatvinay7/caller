cecadfa3d7d2c312d6926521d2d36e13e08fb334
vinay bhat <bhatvinay74@gmail.com>
2025-12-31 00:38:27 +0530

    fix file path

--- apps/web/src/lib/redux/featuresSlice/userDetails.ts
+++ apps/web/src/lib/redux/featuresSlice/userDetails.ts
@@ -1,7 +1,7 @@
 // store/sidebarSlice.ts
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
-import { getUserDetail } from "../../../../../../../payu/apps/web/src/utils/getUserDetail";
+import { getUserDetail } from "../../../utils/getUserDetail";
 import { userCredentials } from "types";
 interface UserState extends userCredentials {
   state?: "pending" | "loading" | "succeeded" | "failed";
