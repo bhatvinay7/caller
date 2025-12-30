81ce0f34e8027765b005a9339090154076654f21
vinay bhat <bhatvinay74@gmail.com>
2025-12-30 20:25:24 +0530

    add user signup and signin

---
+++ apps/http-server/src/controller/user/user.signup.ts
@@ -0,0 +1,21 @@
+import {Response,Request} from "express"
+import { UserSignup, userSchema } from "../../zodvalidator/validator.js"
+import User from "mongodb"
+export const userSignup=async(req:Request,res:Response)=>{
+    try{
+     const data:UserSignup=req.body   
+     const result=userSchema.safeParse(data)
+     if(result.error){
+       return res.status(400).json({message:result.error.issues.map(error=>error.message)})
+     }
+     const user=await User.findOne({email:data.email})
+     if(user){
+        return res.status(409).json({message:["user already exists"]})
+     }
+     await User.create({...data})
+     return res.status(201).json({message:"Signup successful"})
+    }
+    catch(error){
+        return res.status(500).json({message:["server error,please try again!"]})
+    }
+}
---
+++ apps/http-server/src/zodvalidator/validator.ts
@@ -0,0 +1,16 @@
+import {z} from "zod"
+import bcrypt from "bcrypt"
+export const userSchema=z.object({
+    username: z.string().min(2,"Enter your name"),
+    email: z.email({ pattern: z.regexes.email, }),
+    password: z.string().min(6,"Password should have 6 cherater").transform((password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10))),
+})
+
+export const userLoginSchema = z.object({
+  email: z.string().email("Enter a valid email"),
+  password: z.string().min(6, "Password should have at least 6 characters")
+});
+
+// Export inferred TypeScript type
+export type UserLogin = z.infer<typeof userLoginSchema>
+export type UserSignup=z.infer<typeof userSchema>
