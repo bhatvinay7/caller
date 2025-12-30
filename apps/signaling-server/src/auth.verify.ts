9105d3d32e97667fbec76e9dd80d84494b6f03bc
vinay bhat <bhatvinay74@gmail.com>
2025-12-31 00:54:26 +0530

    add auth.verify

---
+++ apps/signaling-server/src/auth.verify.ts
@@ -0,0 +1,15 @@
+import Jwt,{JwtPayload} from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+const JWT_SECRET=process.env.JWT_SECRET!
+export function authVerify(token:string):Jwt.JwtPayload{
+    try{
+     const decode=Jwt.verify(token,JWT_SECRET)
+     return decode as JwtPayload
+    }
+    catch(error){
+        const err=new Error("user is unauthorized")
+        err.name="authError"
+        throw err
+    }
+}
